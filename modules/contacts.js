f2a2ce410ce38793a11df38d8ff9c24b9dfa81ff
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-06-16 19:07:12 -0400

    Model class created for contact list

---
+++ modules/contacts.js
@@ -0,0 +1,16 @@
+
+//require modules for the User Model
+let mongoose = require('mongoose');
+
+//create a model class for contacts list
+let contactsModel = mongoose.Schema({
+    Name: String,
+    Number: String,
+    Email: String
+},
+{
+    collection: "contacts"
+});
+
+//export model
+module.exports = mongoose.model('Contacts', contactsModel);
