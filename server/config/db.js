f42277ae31d9c0f3bebe6870ef14bf35e8763de7
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-06-19 13:56:06 -0400

    URI updated

--- server/config/db.js
+++ server/config/db.js
@@ -3,5 +3,5 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/business_contacts_store"
-    "URI": "mongodb+srv://rodolfoborbon:mocuzarit1992@cluster0.jftooc7.mongodb.net/business_contacts_store?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://rodolfoborbon:teknu6-dibJod-kuqvyn@cluster0.8ndpeek.mongodb.net/business_contacts_store?retryWrites=true&w=majority"
 }
