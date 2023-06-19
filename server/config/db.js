42e7a7f30069721e695b815aa1a29281adc2d44d
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-06-18 20:32:13 -0400

    URI updated to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,6 @@
 // This file provides the URI to connect to the database
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts_store"
+    //"URI": "mongodb://localhost/business_contacts_store"
+    "URI": "mongodb+srv://rodolfoborbon:mocuzarit1992@cluster0.jftooc7.mongodb.net/business_contacts_store?retryWrites=true&w=majority"
 }
